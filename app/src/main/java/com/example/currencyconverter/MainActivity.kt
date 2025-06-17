114007a25a011eb22c59da871c18e245bbfe3ead
leandoero <seventeen.iinc@gmail.com>
2025-06-17 16:08:17 +0300

    completed application mockup (without fonts)

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.currencyconverter
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.navigation.ui.setupWithNavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.currencyconverter.databinding.ActivityMainBinding
@@ -19,5 +20,7 @@ class MainActivity : AppCompatActivity() {
             .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNav.setupWithNavController(navController)
+        window.statusBarColor = ContextCompat.getColor(this, R.color.colorBackground)
+
     }
 }
